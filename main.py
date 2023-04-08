c1c40183bfa7dba68e0609ba9f265bcbcf60cb0c
jayfe <jayfeather233@gmail.com>
2023-04-08 11:20:28 +0800

    update

--- main.py
+++ main.py
@@ -14,17 +14,19 @@ random.seed(0)
 #don't change the class name
 class AI(object):
 
-    dir = np.asarray([(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)])
+    dir = np.asarray([(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)], dtype=int)
 
     map_score = np.asarray(
-[[-64,  1, -10,  -5,  -5, -10,   1,  -64],
-[  1,   3,  -1,  -1,  -1,  -1,   3,    1],
-[-10,  -1,  -3,  -2,  -2,  -3,  -1,  -10],
-[ -5,  -1,  -2,  -1,  -1,  -2,  -1,   -5],
-[ -5,  -1,  -2,  -1,  -1,  -2,  -1,   -5],
-[-10,  -1,  -3,  -2,  -2,  -3,  -1,  -10],
-[  1,   3,  -1,  -1,  -1,  -1,   3,    1],
-[-64,   1, -10,  -5,  -5, -10,   1,  -64]])
+[[-40,   7, -10,  -5,  -5, -10,   7, -40],
+[   7,   5,  -4,  -2,  -2,  -4,   5,   7],
+[ -10,  -4,  -4,  -3,  -3,  -4,  -4, -10],
+[  -5,  -2,  -3,  -2,  -2,  -3,  -2,  -5],
+[  -5,  -2,  -3,  -2,  -2,  -3,  -2,  -5],
+[ -10,  -4,  -4,  -3,  -3,  -4,  -4, -10],
+[   7,   5,  -4,  -2,  -2,  -4,   5,   7],
+[ -40,   7, -10,  -5,  -5, -10,   7, -40]], dtype=float)
+    
+    map_evaluate = np.zeros(shape=(8,8), dtype=float)
 
     #chessboard_size, color, time_out passed from agent
     def __init__(self, chessboard_size, color, time_out):
@@ -32,7 +34,7 @@ class AI(object):
         #You are white or black
         self.color = color
         #the max time you should use, your algorithm's run time must not exceed the time limit.
-        self.time_out = time_out
+        self.time_out = time_out * 1000000000 - 200000000
         # You need to add your decision to your candidate_list. The system will get the end of your candidate_list as your decision.
         self.candidate_list = []
         
@@ -47,11 +49,11 @@ class AI(object):
                     self.history[i][j][k] = tuple((int(k/self.chessboard_size), k%self.chessboard_size))
 
     def in_range(self, pos):
-        return 0<=pos[0] and pos[0]<self.chessboard_size and 0<=pos[1] and pos[1]<self.chessboard_size
+        return 0<=pos[0]<self.chessboard_size and 0<=pos[1]<self.chessboard_size
 
     def check_dir(self, chessboard, pos, di, color):
         # now_pos = pos + di
-        now_pos = tuple(np.add(pos, di))
+        now_pos = (pos[0]+di[0],pos[1]+di[1])
         flg = False
         # print('Before' + str(pos) + str(di))
         while self.in_range(now_pos):
@@ -61,18 +63,18 @@ class AI(object):
                 flg = True
             else:
                 return False
-            now_pos = tuple(np.add(now_pos, di))
+            now_pos = (now_pos[0]+di[0],now_pos[1]+di[1])
             # print(str(now_pos))
         return False
     
 
     def do_move_dir(self, chessboard, pos, di, color):
         # now_pos = pos + di
-        now_pos = tuple(np.add(pos, di))
+        now_pos = (pos[0]+di[0],pos[1]+di[1])
         # print('Before' + str(pos) + str(di))
         while self.in_range(now_pos) and chessboard[now_pos] != color:
             chessboard[now_pos] = color
-            now_pos = tuple(np.add(now_pos, di))
+            now_pos = (now_pos[0]+di[0],now_pos[1]+di[1])
             # print(str(now_pos))
     
 
@@ -82,11 +84,7 @@ class AI(object):
     def evaluate(self, chessboard, color, type = False):
         score = 0
         if not type:
-            # for i in range(8):
-            #     for j in range(8):
-            #         score += chessboard[i][j] * self.map_score[i][j]
             score = np.sum(chessboard * self.map_score)
-            # score += (len(self.get_candidate(chessboard, color)) - len(self.get_candidate(chessboard, -color)))*7
         else:
             score = np.sum(chessboard) * -1
         return score * color
@@ -118,78 +116,98 @@ class AI(object):
         
         player = self.color
         
-        def max_value(chessboard, id, step, alpha, beta, is_skiped, evaluate_type):
+        def max_value(chessboard, len_player, len_nplayer, step, alpha, beta, is_skiped, evaluate_type):
+            if self.cnt % 1000 == 0:
+                if time.time_ns() - self.start_time > self.time_out:
+                    # print(self.cnt, time.time_ns(), '\n')
+                    return self.evaluate(chessboard, player, evaluate_type), None
+            self.cnt += 1
             # print(chessboard)
-            if step == 0:
+            if step == 0: # or (time.thread_time_ns() - self.start_time) > self.time_out:
                 return self.evaluate(chessboard, player, evaluate_type), None
-            actions = self.get_candidate(chessboard, player)
-            if len(actions) == 0:
-                if is_skiped:
-                    return self.evaluate(chessboard, player, True) * 1000, None # end state
-                else:
-                    return min_value(chessboard, id, step, alpha, beta, True, evaluate_type)[0], None
+
             v, move = -math.inf, None
             for i in range(64):
-                a = self.history[0][id][i]
+                a = self.history[0][len_player][i]
                 # print(a)
-                if not a in actions:
+                if chessboard[a] != COLOR_NONE or not self.check(chessboard, a, player):
+                # if not (a in actions):
                     continue
-                v2, _ = min_value(self.do_move(chessboard, a, player), id, step - 1, alpha, beta, False, evaluate_type)
+                v2, _ = min_value(self.do_move(chessboard, a, player), len_nplayer, len_player + 1, step - 1, alpha, beta, False, evaluate_type)
+                # if step == 6:
+                #     self.map_evaluate[a] = v2
                 if v < v2:
                     v, move = v2, a
                 
                 if v>= beta:
-                    self.float_move(0,id,i)
+                    self.float_move(0,len_player,i)
                     break
                 if alpha < v:
-                    self.advantage_move(0,id,i)
+                    self.advantage_move(0,len_player,i)
                     alpha = v
+            
+            if move == None:
+                if is_skiped:
+                    return self.evaluate(chessboard, player, True) * 10000, None # end state
+                else:
+                    return min_value(chessboard, len_nplayer, len_player, step, alpha, beta, True, evaluate_type)[0], None
                     
             return v, move
                 
                 
-        def min_value(chessboard, id, step, alpha, beta, is_skiped, evaluate_type):
-            if step == 0:
+        def min_value(chessboard, len_player, len_nplayer, step, alpha, beta, is_skiped, evaluate_type):
+            if self.cnt % 1000 == 0:
+                if time.time_ns() - self.start_time > self.time_out:
+                    # print(self.cnt, time.time_ns(), '\n')
+                    return self.evaluate(chessboard, player, evaluate_type), None
+            self.cnt += 1
+            
+            if step == 0: # or (time.thread_time_ns() - self.start_time) > self.time_out:
                 return self.evaluate(chessboard, player, evaluate_type), None
-            actions = self.get_candidate(chessboard, -player)
-            if len(actions) == 0:
-                if is_skiped:
-                    return self.evaluate(chessboard, player, True) * 1000, None # end state
-                else:
-                    return max_value(chessboard, id + 1, step, alpha, beta, True, evaluate_type)[0], None
+            
             v, move = math.inf, None
             for i in range(64):
-                a = self.history[1][id][i]
-                if not a in actions:
+                a = self.history[1][len_player][i]
+                if chessboard[a] != COLOR_NONE or not self.check(chessboard, a, -player):
+                # if not (a in actions):
                     continue
-                v2, _ = max_value(self.do_move(chessboard, a, -player), id + 1, step - 1, alpha, beta, False, evaluate_type)
+                v2, _ = max_value(self.do_move(chessboard, a, -player), len_nplayer, len_player + 1, step - 1, alpha, beta, False, evaluate_type)
                 if v > v2:
                     v, move = v2, a
                     
                 if v <= alpha:
-                    self.float_move(1, id, i)
+                    self.float_move(1, len_player, i)
                     break
                 if beta > v:
-                    self.advantage_move(1, id, i)
+                    self.advantage_move(1, len_player, i)
                     beta = v
+                    
+            if move == None:
+                if is_skiped:
+                    return self.evaluate(chessboard, player, True) * 10000, None # end state
+                else:
+                    return max_value(chessboard, len_nplayer, len_player, step, alpha, beta, True, evaluate_type)[0], None
                     
             return v, move
         
-        l = int(len(np.where(chessboard == player)[0]))
+        # l = int(len(np.where(chessboard == player)[0]))
+        # len_player = int(len(np.where(chessboard == player)[0]))
+        # len_nplayer = int(len(np.where(chessboard == -player)[0]))
+        len_player = len_nplayer = int((64 - int(len(np.where(chessboard == COLOR_NONE)))) / 2)
         # print(l)
-        return max_value(chessboard, l, step, -math.inf, math.inf, False, False)
+        return max_value(chessboard, len_player, len_nplayer, step, -math.inf, math.inf, False, False)
     
     def get_candidate(self, chessboard, color):
-        idx = np.where(chessboard == COLOR_NONE)
-        idx = list(zip(idx[0], idx[1]))
         mlist = list()
-        for u in idx:
-            if self.check(chessboard, u, color):
-                mlist.append(u)
+        for x1 in range(self.chessboard_size):
+            for x2 in range(self.chessboard_size):
+                if chessboard[x1][x2]==COLOR_NONE and self.check(chessboard, (x1,x2), color):
+                    mlist.append((x1,x2))
         return mlist
 
     # The input is the current chessboard. Chessboard is a numpy array.
     def go(self, chessboard):
+        self.map_evaluate.fill(0)
         # Clear candidate_list, must do this step
         self.candidate_list.clear()
         #==================================================================
@@ -197,14 +215,45 @@ class AI(object):
         #Here is the simplest sample:Random decision
 
         self.candidate_list = self.get_candidate(chessboard, self.color)
-
-        if len(np.where(chessboard == COLOR_NONE)[0]) < 10:
-            move = self.min_max(chessboard, 10)[1]
-        else:
-            # move = self.ID_minmax(chessboard, self.time_out)
-            move = self.min_max(chessboard, 6)[1]
-        if move != None:
-            self.candidate_list.append(move)
+        
+        self.start_time = time.time_ns()
+        self.cnt = 0
+        # print('time:', self.start_time)
+        
+        l = len(np.where(chessboard == COLOR_NONE)[0])
+        low_bound = 10 if l <= 13 else 6
+        low_bound = min(low_bound, l)
+        
+        for k in range(low_bound, l+2):
+            move = self.min_max(chessboard, k)[1]
+            if time.time_ns() - self.start_time > self.time_out:
+                print(k, self.cnt, (time.time_ns() - self.start_time) / 1000000000, '\n')
+                break
+            if move != None:
+                self.candidate_list.append(move)
+        
+        
+        # low_bound = 3
+        # k = len(np.where(chessboard == COLOR_NONE)[0])
+        # if k < 13:
+        #     low_bound = 13
+        # else:
+        #     low_bound = 7
+        # las_move = None
+        # move = None
+        # move = self.min_max(chessboard, low_bound - 2)[1]
+        # las_move = move
+        # if las_move != None:
+        #     self.candidate_list.append(las_move)
+        
+        # move = self.min_max(chessboard, low_bound)[1]
+        # las_move = move
+        # if las_move != None:
+        #     self.candidate_list.append(las_move)
+        
+        
+        
+        # print('time:', time.thread_time_ns())
         #==============Find new pos========================================
         # Make sure that the position of your decision on the chess board is empty. 
         # If not, the system will return error.
--- test.py
+++ test.py
@@ -1,14 +1,53 @@
 import main
 import numpy as np
 
+def print_board(chessboard):
+    print(end='  ')
+    for i in range(8):
+        print(i, end=' ')
+    print('\n', end='')
+    for i in range(8):
+        for j in range(8):
+            if j ==0:
+                print(i, end=' ')
+            if chessboard[i][j] == 1:
+                print("O ", end='')
+            elif chessboard[i][j] == -1:
+                print("X ", end='')
+            else:
+                print("  ", end='')
+        print('\n', end='')
+
+def print_evaluate(eva):
+    print()
+    print(end='  ')
+    for i in range(8):
+        print(i, end=' ')
+    print('\n', end='')
+    for i in range(8):
+        for j in range(8):
+            print(eva[i][j], end=' ')
+        print('\n', end='')
+
 if __name__ == "__main__":
-    agent = main.AI(8,1,10)
+    agent1 = main.AI(8,1,5)
+    agent2 = main.AI(8,-1,5)
     cb = np.zeros(shape=(8,8))
     cb[(3,3)]=1
     cb[(4,4)]=1
     cb[(3,4)]=-1
     cb[(4,3)]=-1
-    print(cb)
-    agent.go(cb)
-    print(agent.candidate_list)
-    print(agent.do_move(cb, (3,4), -1))
+    while True:
+        print_board(cb)
+        # x=int(input())
+        # y=int(input())
+        # cb = agent.do_move(cb, (x,y), -1)
+        agent1.go(cb)
+        # print_evaluate(agent.map_evaluate)
+        # print(agent.candidate_list)
+        cb = agent1.do_move(cb, agent1.candidate_list[len(agent1.candidate_list)-1], 1)
+        print_board(cb)
+        agent2.go(cb)
+        # print_evaluate(agent.map_evaluate)
+        # print(agent.candidate_list)
+        cb = agent1.do_move(cb, agent2.candidate_list[len(agent2.candidate_list)-1], -1)
